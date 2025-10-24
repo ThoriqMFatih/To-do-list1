053c265a936179d073518c4345af35c3cc5e4b8e
ThoriqMFatih <thoriqmf3009@gmail.com>
2025-10-24 10:36:44 +0700

    progress module 2

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,34 @@ function App() {
     time : "10 AM",
     status : "Active"
   },
+   {
+    Title : "Physics class",
+    desc : "attending the Physics class",
+    date : "28 january",
+    time : "10 AM",
+    status : "Active"
+  },
+  {
+    Title : "calculus class",
+    desc : "attending the calculus class",
+    date : "25 january",
+    time : "10 AM",
+    status : "Active"
+  },
+  {
+    Title : "Physics class",
+    desc : "attending the Physics class",
+    date : "28 january",
+    time : "10 AM",
+    status : "Active"
+  },
+   {
+    Title : "Physics class",
+    desc : "attending the Physics class",
+    date : "28 january",
+    time : "10 AM",
+    status : "Active"
+  },
 ];
 
 const divArray = [];
@@ -22,7 +50,7 @@ const divArray = [];
 for (let i=0; i<task.length; i++ ){
 let currentTask = task[i];
 divArray.push (
-            <div className="bg-slate-700 p-4 rounded-2xl shadow-lg flex-col">
+            <div className="bg-white p-4 rounded-2xl shadow-lg flex-col text-black basis-[48.5%] m-[0.75%]">
               <p className="font-semibold text-2xl">{currentTask.Title}</p>
 
               <p className="text-base">{currentTask.desc}</p>
@@ -41,15 +69,15 @@ divArray.push (
 }
 
   return (
-    <div className="flex justify-center w-full min-h-screen bg-gray-800 text-white">
-      <header className="absolute top-0 text-xl p-5 bg-gray-600 w-full text-center rounded-lg">To-Do List App</header>
+    <div className="flex justify-center w-full min-h-screen bg-red-700 text-white p-5">
+      <header className="shadow-lg absolute top-0 text-xl p-5 bg-red-600 w-full text-center font-BBH italic">To-Do List App</header>
 
       <main className="pt-36 w-3/4">
         {/* User Prompt */}
         <div className="flex justify-center">
-          <input className="bg-slate-700 p-4 rounded-2xl w-3/4 shadow-md" placeholder="Type your task here..." />
+          <input className="bg-white p-4 rounded-2xl w-3/4 shadow-md text-black" placeholder="Type your task here..." />
           <button className="pl-2 h-12 pt-2">
-            <img src="/logo192.png" alt="enter" className="w-full h-full" />
+            <img src="/Tomboll.png" alt="enter" className="size-full" />
           </button>
         </div>
 
@@ -59,10 +87,12 @@ divArray.push (
         {/*To do List*/}
         <div className="flex justify-center">
           <div className="w-[85%] flex flex-col gap-y-4">
-            <p className="font-semibold text-xl">Your To-Do List</p>
+            <p className="font-bold text-xl font-sans text-center">Your To-Do List</p>
             <hr />
 
-            {divArray}
+            <div class="flex flex-row flex-wrap">
+              {divArray}
+            </div>
            
           </div>
         </div>
